3b0205aa9ae804e17a22c7a9bac6e8ef3fb38c7c
Sunilutr12 <kumaradb12@gmail.com>
2022-04-09 13:25:08 +0530

    Add header componet styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import  "./styles.css"
  class Header extends React.Component{
 
     render(){
         return(
-            <div>
+            <div className='header'>
             <h1>Welcome to Space -X</h1>
             <p> My name is sumeil kumar</p>
             <p> My Village is Kothagudem </p>
