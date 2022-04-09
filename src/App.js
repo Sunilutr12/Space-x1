28b570732dd1e1f6b511276234cc5190397c0403
Sunilutr12 <kumaradb12@gmail.com>
2022-04-09 14:05:00 +0530

    Add Lanuch Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 import React from 'react'
 import Header from './components/header'
 import './style.css'
+import{Launch} from './components/Launch/Launch'
 
 function App() {
   return (
     <div className="App">
     <center>
       <Header/>
+      <Launch/>
+
       </center>
     </div>
   );
---
+++ src/components/Launch/Launch.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export class Launch extends React.Component{
+
+    render(){
+        return(
+            <div>
+            <img src="https://farm9.staticflickr.com/8637/16079045013_1f0fab9b54_o.jpg"/>
+            <h1>Title:FalconSat</h1>
+            <h1>Date:2006-03-25T10:30:00+12:00</h1>
+            <p>Descriptyion:"Engine failure at 33 seconds and loss of vehicle",</p>
+            </div>
+        )
+    }
+}
