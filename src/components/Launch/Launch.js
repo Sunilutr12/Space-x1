4a3233769c1d6292f674526c6016e7ad1d417364
Sunilutr12 <kumaradb12@gmail.com>
2022-04-10 08:34:41 +0530

    Added styles

--- src/components/Launch/Launch.js
+++ src/components/Launch/Launch.js
@@ -8,10 +8,10 @@ export class Launch extends React.Component{
             <img src="https://farm9.staticflickr.com/8637/16079045013_1f0fab9b54_o.jpg"/>
             <div className='content'>
             <div className='info'>
-            <h1>Title:FalconSat</h1>
-            <h1>Date:2006-03-25T10:30:00+12:00</h1>
+               <h1>Title:FalconSat</h1>
+               <h1>Date:2006-03-25T10:30:00+12:00</h1>
             </div>
-            <p>Descriptyion:"Engine failure at 33 seconds and loss of vehicle",</p>
+            <p>Descriptyion:"Engine failure at 33 seconds and loss of vehicle""Engine failure at 33 seconds and loss of vehicle""Engine failure at 33 seconds and loss of vehicle""Engine failure at 33 seconds and loss of vehicle""Engine failure at 33 seconds and loss of vehicle""Engine failure at 33 seconds and loss of vehicle",</p>
             </div>
             </div>
         )
